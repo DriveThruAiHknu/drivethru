b67e823d4ce9ba38642d41ae990b080f2c833ca7
박민수 <minsoo3066@naver.com>
2022-02-14 19:04:03 +0900

    database model

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'driveapp.apps.DriveappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
