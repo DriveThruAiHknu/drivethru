aa17c06e76b599c849bf55158b292d3b908154e5
eunsoo <skfmf7878@naver.com>
2022-07-06 00:17:14 +0900

    형태소 합침/고객 주문 리스트 구현

--- driveapp/clienturls.py
+++ driveapp/clienturls.py
@@ -7,9 +7,5 @@ urlpatterns = [
     path('menu/member-orders', views.member_order),
     path('menu/non-member-orders', views.non_member_order),
     path('end', views.user_end),
-    path('stt', views.stt),
-    path('tts', views.tts),
-    path('play',views.audio_play),
-    path('voice1', views.voice_recognize1),
-    path('voice2', views.voice_recognize2),
+    path('analyzer', views.stem_analyzer),
 ]
--- driveapp/views.py
+++ driveapp/views.py
@@ -7,23 +7,28 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.http import JsonResponse
 
-# Create your views here.
 
-# 1) 관리자  
+"""
+1) 관리자
+""" 
 def manage_login(request):
     context = {
         'a':''
     }
     return render(request, 'manager/manage_login.html', context)
+
+
 def manage_menu(request):    
     class_prods = prod.objects.all()
-
     return render(request, 'manager/manage_menu.html', {'class_prods':class_prods})
+
+
 def manage_menuadd(request):
     context = {
         'a':''
     }
     return render(request, 'manager/manage_menu_add.html', context)
+
 
 def manage_recommendation_menu(request):
     context = {
@@ -31,11 +36,13 @@ def manage_recommendation_menu(request):
     }
     return render(request, 'manager/manage_recommendation_menu.html', context)
     
+
 def manage_orders(request):
     context = {
         'a':''
     }
     return render(request, 'manager/manage_orders.html', context)
+
 
 def staff_orders(request):
     context = {
@@ -43,14 +50,20 @@ def staff_orders(request):
     }
     return render(request, 'manager/staff_orders.html', context)
 
-# 2) 고객
+
+
+"""
+2) 고객
+"""
+
+
+# 1. 차 조회
 from drive_restapi.models import member, today_user
 members = member.objects.all() #member 데이터 db에서 가져오기
 
 def user_car(request):
     context = {
-        'a'
-        :''
+        'a':''
     }
 
     #POST 전송이 들어오면
@@ -76,295 +89,20 @@ def user_car(request):
                 return redirect('/client/menu/non-member-orders')
         #return redirect('/client/menu/member-orders') #같은 앱 내 주소로 이동
         
-    return render(request, 'client/user_car.html', context)   
+    return render(request, 'client/user_car.html', context)
 
 
+# 2. 고객 주문
+from drive_restapi.models import prod
+prods = prod.objects.all() #prod 데이터 db에서 가져오기
+prods_val = prod.objects.values_list()
+prod_list = list(prods_val)
+print(prod_list)
 
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-어서오세요. 스타벅스입니다~
-주문을 원하시는 메뉴나 번호를 말씀해주세요
-
-1) 메뉴 말할 경우
-~ 메뉴를 선택하셨습니다. 결제 또는 추가할 메뉴를 말씀해주세요! (다시 주문 입력 대기)
-
-2) 메뉴 하나 주문 후 30초 이상 침묵일 경우
-결제 안내를 도와드리겠습니다 (주문 종료로 인식)
-
-3) 메뉴 아무것도 주문하지 않고 30초 이상 침묵일 경우
-주문 받을 때까지 대기
-
-4) 결제 말할 경우
-결제 안내를 도와드리겠습니다 (주문 종료로 인식)
-
-5) TTS 결과 실패할 경우
-주문을 인식하지 못했습니다. 다시 한번 말씀해주세요! (다시 주문 입력 대기)
-
-"""
-
-"""
-
-[ 오디오 녹음 함수 ]
-:  해당 함수는 시작부터 쭉 녹음해서 말이 끝나고 4초가 지나면 오디오 파일로 저장해줌.
-
-0. 변수 (침묵시간 sielence) (한 번이라도 말했는지 여부 audio_once)
-1. stream 열고, stream으로 부터 읽어서 Frame에 추가
-2. 500 이상의 소리 크기가 감지되면 silence(침묵 시간 초기화), audio_once True(한번이라도 말함)
-3. 아니라면 silence++ (침묵시간 증가), continue(루프 조건으로)
-4. 한번이라도 말을 했고, 침묵시간이 50(약 4초) 지나면 break(종료)
-
-=> True 반환.
-
-"""
-
-def mic(FORMAT, CHANNELS, RATE, CHUNK, INPUT_DEVICE_INDEX, WAVE_OUTPUT_FILENAME):
-
-    # 변수
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 16000
-    CHUNK = 1024
-    INPUT_DEVICE_INDEX = 1 #4 (error) -> 1 (success)
-    WAVE_OUTPUT_FILENAME = "주문.wav"
-    audio_once = False
-    audio = pyaudio.PyAudio()
-    # 녹음 시작
-    logging.info("---녹음 시작")
-    stream = audio.open(format=pyaudio.paInt16, channels=CHANNELS, rate=RATE, input=True, input_device_index=INPUT_DEVICE_INDEX,
-                        frames_per_buffer=CHUNK)
-
-    frames = []
-    silence = 0
-    audio_once = False #한번이라도 말했으면
-    
-    while True:
-        if (silence > 43) & (audio_once):
-            break
-            
-        data = stream.read(CHUNK) #음성 데이터 스트림으로부터 읽어오기
-        frames.append(data) #스트림 -> 프레임에 추가
-        
-        data_chunk = array('h', data) #볼륨 측정
-        vol = max(data_chunk) #볼륨 측정
-        
-        if(vol >= 500):
-            silence = 0
-            audio_once = True
-        else:
-            silence += 1
-            continue
-        
-    
-    logging.info("녹음 끝---\n")
-
-     #녹음 끝
-
-    stream.stop_stream()
-    stream.close()
-
-    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    waveFile.setnchannels(CHANNELS)
-    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-    waveFile.setframerate(RATE)
-    waveFile.writeframes(b''.join(frames))
-    waveFile.close()
-    
-    return True
-
-"""
-
-카카오 STT 함수
-: 해당 함수는 오디오 파일을 텍스트로 변환 요청해주는 함수.
-
-=> 요청 결과 코드와 값 = res.text = result
-=> 타입 확인해서 성공하면, 성공한 결과 인식 값 반환
-=> 실패하면, "주문을 인식하지 못했습니다." 반환
-
-"""
-
-def stt(request):
-
-    url = "https://kakaoi-newtone-openapi.kakao.com/v1/recognize"
-    key = '51563f7b6dec012e347135fc5bc07bb7'
-    headers = {
-        "Content-Type": "application/octet-stream",
-        #"Transfer-Encoding":"chunked",
-        "Authorization": "KakaoAK " + key,
-    }
-
-    with open(request.GET['file_name']+".wav", 'rb') as fp:
-        audio = fp.read()
-
-    res = requests.post(url, headers=headers, data=audio)
-    result = res.text
-
-    #음성 인식 끝나고 반환 결과 있으면 Type 가져오기
-    if result.find("type") != -1:
-        type_str = result.find("type") + 7
-        type_end = result.find("value") - 3
-        result_type = result[type_str:type_end]
-    
-     #Type이 실패한 결과라면
-    if result_type == "errorCalled": 
-        val = "주문을 인식하지 못했습니다."
-
-    #Type이 성공한 결과라면   
-    else:
-        val_str = result.find('"type":"finalResult"') + 30
-        val_end = result.find("nBest") - 3
-        val = result[val_str:val_end] #필요한 텍스트만 JSON에서 추출
-        val_json = {"val":val}
-    return JsonResponse(val_json)
-
-
-def audio_play(request):
-    playsound("tts"+request.GET['file_name']+".mp3")
-    time.sleep(0.2)
-    #os.remove("tts"+".mp3")
-    return HttpResponse("tts"+request.GET['file_name']+".mp3"+" 실행 성공")
-    
-"""
-
-카카오 TTS 함수
-: 해당 함수는 텍스트를 오디오 파일로 합성해주는 함수
-
-=> 음성 파일 이름 : "tts+번호.mp3"
-
-"""
-import pyaudio
-import wave
-from array import array
-import numpy as np
-import requests, json
-import time
-
-import requests
-import io
-import os
-from playsound import playsound
-import logging
-logging.basicConfig(level='DEBUG')
-
-def tts(request):
-    url = "https://kakaoi-newtone-openapi.kakao.com/v1/synthesize"
-    key = '51563f7b6dec012e347135fc5bc07bb7'
-    headers = {
-        "Content-Type" : "application/xml",
-        "Authorization" : "KakaoAK " + key,
-    }
-
-    data = "<speak>"+str(request.GET['text'])+"</speak>"
-    
-    res = requests.post(url, headers = headers, data = data.encode('utf-8'))
-    
-    with open("tts"+".mp3", "wb") as f:
-        f.write(res.content)
-    
-    playsound("tts"+".mp3")
-    time.sleep(0.2)
-
-    os.remove("tts"+".mp3")
-
-    return HttpResponse("tts"+request.GET['text']+".mp3"+" 실행 성공")
-
-#처음 음성인식 확인할 때 -> 초수 안세도 되고, 무조건 들어오면 바로 녹음
-def voice_recognize1(request):
-
-    # 변수
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 16000
-    CHUNK = 1024
-    INPUT_DEVICE_INDEX = 1 #4 (error) -> 1 (success)
-    WAVE_OUTPUT_FILENAME = "주문"
-    voice_result = ""
-    audio = pyaudio.PyAudio()
-    
-
-    # 크기 측정
-    while True:
-
-        stream_vol = audio.open(format=pyaudio.paInt16, channels=CHANNELS, rate=RATE, input=True, 
-                    input_device_index=INPUT_DEVICE_INDEX,frames_per_buffer=CHUNK)
-
-        #크기 측정
-        data_vol=stream_vol.read(CHUNK)
-        data_chunk_vol=array('h',data_vol)
-        vol=max(data_chunk_vol)
-
-        #목소리 감지
-        if(vol>=500):
-            logging.info("인식중...\n")
-            audio_file = mic(FORMAT, CHANNELS, RATE, CHUNK, INPUT_DEVICE_INDEX, WAVE_OUTPUT_FILENAME+str(1)+".wav")
-            
-            #오디오파일 저장 성공했으면
-            voice_result = "저장" #오디오 저장함
-            audio.terminate()
-            return HttpResponse(voice_result) #저장
-
-
-
-#두번째 음성녹음 실행 -> 초수 재서 몇 초 이상이면 그냥 끝내고 넘어가기
-def voice_recognize2(request):
-
-    # 변수
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 16000
-    CHUNK = 1024
-    INPUT_DEVICE_INDEX = 1 #4 (error) -> 1 (success)
-    WAVE_OUTPUT_FILENAME = "주문"
-    voice_result = ""
-    silence_real = 0
-    audio = pyaudio.PyAudio()
-    
-
-    # 크기 측정
-    while True:
-
-        stream_vol = audio.open(format=pyaudio.paInt16, channels=CHANNELS, rate=RATE, input=True, 
-                    input_device_index=INPUT_DEVICE_INDEX,frames_per_buffer=CHUNK)
-
-        #크기 측정
-        data_vol=stream_vol.read(CHUNK)
-        data_chunk_vol=array('h',data_vol)
-        vol=max(data_chunk_vol)
-
-        #목소리 감지
-        if(vol>=500):
-            logging.info("인식중...\n")
-            audio_file = mic(FORMAT, CHANNELS, RATE, CHUNK, INPUT_DEVICE_INDEX, WAVE_OUTPUT_FILENAME+str(1)+".wav")
-            
-            #오디오파일 저장 성공했으면
-            voice_result = "저장" #오디오 저장함
-            silence_real = 0 #침묵 시간 초기화
-            audio.terminate()
-            return HttpResponse(voice_result) #저장
-
-        else:
-            if (silence_real > 90):
-                voice_result="대기초과" 
-                audio.terminate()
-                return HttpResponse(voice_result) #끝
-
-            silence_real += 1
 
 def member_order(request):
-    context = {
-        'a':''
-    }
-    return render(request, 'client/member_order.html', context)
+    #context = { 'a':''}
+    return render(request, 'client/member_order.html', {'prods':prod_list})
 
 
 def non_member_order(request):
@@ -373,8 +111,97 @@ def non_member_order(request):
     }
     return render(request, 'client/non_member_order.html', context) 
 
+
 def user_end(request):
     context = {
         'a':''
     }
-    return render(request, 'client/user_end.html', context)
+    return render(request, 'client/user_end.html', context)\
+
+
+
+#3) 형태소 분석기
+
+
+"""
+형태소 분석기
+"""
+
+from ckonlpy.tag import Postprocessor
+from ckonlpy.tag import Twitter
+import pandas as pd
+from ckonlpy.utils import load_wordset
+from ckonlpy.utils import load_replace_wordpair
+from ckonlpy.utils import load_ngram
+
+from django.http import JsonResponse
+
+"""
+from rest_framework.decorators import api_view
+@api_view(['GET'])
+"""
+
+def stem_analyzer(requests):
+    print("stt로 받은 문자열")
+    print(requests.GET['resText'])
+    sent = requests.GET['resText']
+
+    twitter = Twitter()
+
+    #메뉴 추가하기
+    cafemenu = pd.read_csv('cafemenu.csv')
+    cafemenu['이름'].dropna()
+
+    menudata=cafemenu['이름'].str.replace(' ', '')#데이터 형식 변경
+    for i in menudata:
+        twitter.add_dictionary(i, 'Noun')
+
+    menudata=cafemenu['이름']
+    for i in menudata:
+        twitter.add_dictionary(i, 'Noun')
+        
+    #옵션 종류 추가하기
+    cafeoption = pd.read_csv('cafeoption.csv')
+    cafeoption['옵션 종류'].dropna()
+    optiondata=cafeoption['옵션 종류'].str.replace(' ', '')
+
+    for i in optiondata:
+        twitter.add_dictionary(i, 'Noun')
+        
+    optiondata=cafeoption['옵션 종류']
+    for i in optiondata:
+        twitter.add_dictionary(i, 'Noun')
+ 
+    #문장에 필요없는 단어
+    stopwords = load_wordset('.\\tutorials\\stopwords.txt')
+
+    #의미가 같은 것으로 변경할 단어
+    replace = load_replace_wordpair('.\\tutorials\\replacewords.txt')
+
+    #띄어쓰기 없애는 단어로
+    ngrams = load_ngram('.\\tutorials\\ngrams.txt')
+
+    postprocessor = Postprocessor(
+        base_tagger = twitter, # base tagger
+        stopwords = stopwords, # 해당 단어 필터
+        #passwords = passwords, # 해당 단어만 선택
+        #passtags = passtags, # 해당 품사만 선택
+        ngrams = ngrams, # 해당 복합 단어 set을 한 단어로 결합
+        replace = replace # 해당 단어 set 치환
+    )
+    orderdata = []
+    order = []
+    delete_word = [' ', '-']
+
+    for i in postprocessor.pos(sent):
+        orderdata.append(i[0])
+
+    print("단어만 추가: ", orderdata)
+    for i in orderdata:
+        for j in delete_word:
+            i=i.replace(j, '')
+        order.append(i)
+    print("\n")
+    print("형태소 분석한 문장:", order)
+
+    return JsonResponse(order, safe=False)
